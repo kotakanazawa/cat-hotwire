bd7cb7097775ffb582e2365e190da9d4e01d1927
Kota Kanazawa <kota0818@gmail.com>
2024-10-10 20:45:46 +0900

    FlashのTurbo Streams化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -42,7 +42,7 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "Cat was successfully updated.", status: :see_other
+      flash.now.notice = "Cat was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
