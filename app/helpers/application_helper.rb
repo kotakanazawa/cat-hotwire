52a334746567b9c1907c2e8b2a0f4add0454958c
Kota Kanazawa <kota0818@gmail.com>
2024-10-10 21:04:28 +0900

    flashの更新をヘルパーに切り出す

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,17 @@ module ApplicationHelper
       tag.span(class: "me-2") { emoji } + tag.span { text }
     end
   end
+
   def icon(icon_name)
     tag.i(class: ["bi", "bi-#{icon_name}"])
   end
   def icon_with_text(icon_name, text)
     tag.span(icon(icon_name), class: "me-2") + tag.span(text)
+  end
+
+  def turbo_stream_flash
+    turbo_stream.update "flash" do
+      render partial: "flash"
+    end
   end
 end
